210ac03cea7b7b527413b0633a6573c5fae0a4c8
Hígialuz Tecnologia <emerson@higialuz.com.br>
2022-01-12 20:18:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I Will Learn React!
         </a>
       </header>
     </div>
